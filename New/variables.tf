a8095da4822c67e1ca1ba28fd96ce3ba31e4db32
enz2g <brad.jc@proton.me>
2025-09-18 22:00:10 +1000

    terraform updates

--- New/variables.tf
+++ New/variables.tf
@@ -1,19 +1,4 @@
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-}
 
-variable "client_id" {
-  description = "Azure Client ID"
-}
-
-variable "client_secret" {
-  description = "Azure Client Secret"
-  sensitive   = true
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-}
 
 variable "vault_name" {
   description = "Key Vault name"
